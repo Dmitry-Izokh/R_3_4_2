b95ec7435108f7082749b0bb747b2bf9f5c03cd8
Дмитрий Изох <izokh_dv@mail.ru>
2023-01-25 23:15:59 +0300

    Добавьте файлы проекта.

---
+++ R_3_4_2/FiltredElementCollector.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.DB;
+using System;
+
+namespace R_3_4_2
+{
+    internal class FiltredElementCollector
+    {
+        private Document doc;
+
+        public FiltredElementCollector(Document doc)
+        {
+            this.doc = doc;
+        }
+
+        internal object OfCategory(BuiltInCategory oST_Rooms)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
